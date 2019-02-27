288b221592336fdf82354e0166c7fd753ef68721
tbuffington7 <tyler.c.buffington@utexas.edu>
2019-02-26 22:45:11 -0600

    added exercise 3

--- exercises_02/gibbs_sampler.py
+++ exercises_02/gibbs_sampler.py
@@ -1,16 +1,17 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import scipy.stats as stats
+from numpy.linalg import inv
 import pdb
-
 
 
 '''
 Recap of definitions derived in written notes:
-m_star = np.linalg.inv(x.transpose()@Lambda@x + K) @ (x.transpose() @ Lambda @ y + K @ m)
-K_star = np.linalg.inv(x.transpose()@Lambda@x + K)
+m_star = inv(np.transpose(x)@Lambda@x + K) @ (np.transpose(x) @ Lambda @ y + K @ m)
+K_star = np.transpose(x)@Lambda@x + K
 d_star = d + n
-eta_star = eta + y.transpose() @ Lambda @ y + m.transpose() @ K @ m - m_star.transpose() @ K_star @ m 
+eta_star = eta + np.transpose(y) @ Lambda @ y + np.transpose(m) @ K @ m - m_star.transpose() @ K_star @ m 
 
 
 
@@ -22,69 +23,72 @@ p(l[i], y, beta, w ) ~ Gamma((h+1)/2, w/2.0*(y - x@beta)**2 + h/2.0)
 '''
 
 
-def gibbs_sampler(x,y,iterations=1000,burn=100,thin=2, h=0.01, Kvals = 0.01, d = 0.01, eta=0.01, w=1.0):
+def gibbs_sampler(x,y,iterations=1000,burn=100, h=1.0, Kvals = 0.01, d = 1.0, eta=1.0, w=1.0):
 
-	n = len(y) 
-	p = len(x.columns)
+    n = len(y) 
+    p = len(x.columns)
 
-	#initialize parameters
-	K = np.diag(np.ones(p))*Kvals #A vague prior for the K precision
-	m = np.zeros(p)
-	Lambda = np.diag(np.ones(n)) * 0.1
-	beta = np.zeros(p)
+    #initialize parameters
+    K = np.diag(np.ones(p))*Kvals #A vague prior for the K precision
+    m = np.zeros(p)
+    Lambda = np.diag(np.ones(n))
+    beta = np.zeros(p)
 
-	#Storing the results in a dictionary
-	results = {
+    #Storing the results in a dictionary
+    results = {
 
-			'beta_trace': np.zeros([iterations, p]),
-			'Lambda_trace': np.zeros([iterations, n]),
-			'w_trace': np.zeros(iterations),
+            'beta_trace': np.zeros([iterations, p]),
+            'Lambda_trace': np.zeros([iterations, n]),
+            'w_trace': np.zeros(iterations),
 
-	}
+    }
 
+    for i in range(iterations):
 
+        #Update beta
+        m_star = inv(np.transpose(x) @ Lambda @ x + K) @ (np.transpose(x) @ Lambda @ y + K @ m)
+        pdb.set_trace()
+        K_star = np.transpose(x) @ Lambda @ x + K
+        beta = stats.multivariate_normal.rvs(mean = m_star, cov=inv(w*K_star))
 
-	for i in range(iterations):
+        #Update w
+        d_star = d + n
+        eta_star = eta + np.transpose(y) @ Lambda @ y + np.transpose(m) @ K @ m - np.transpose(m_star) @ inv(K_star) @ m_star
+        w = stats.gamma.rvs(d_star/2.0, (2.0/eta_star))
 
-		#Update beta
-		m_star = np.linalg.inv(x.transpose()@Lambda@x + K) @ (x.transpose() @ Lambda @ y + K @ m)
-		K_star = np.linalg.inv(x.transpose()@Lambda@x + K)
-		beta = np.random.multivariate_normal(m_star, np.linalg.inv(w*K_star))
+        #Update lambda
+        # Lambda = np.diag([stats.gamma.rvs((h+1.0)/2.0, (w/2.0*(y[j] - np.transpose(x.iloc[j])@beta)**2 + h/2.0)**-1.0)  for j in range(n)])
 
-		#Update lambda
-		Lambda = np.diag(np.random.gamma((h+1.0)/2.0, (w/2.0*(y - x@beta)**2 + h/2.0)**-1 ))
-
-		#Update w
-		d_star = d + n
-		eta_star = eta + y.transpose() @ Lambda @ y + m.transpose() @ K @ m - m_star.transpose() @ K_star @ m 
-		w = np.random.gamma(d_star/2.0, 2.0/eta_star)
-
-		results['beta_trace'][i,:] = beta
-		results['Lambda_trace'][i,:] = np.diagonal(Lambda)
-		results['w_trace'][i] = w
-
-
-		print('{:d} iterations out of {:d} complete.'.format(i,iterations))
-
-	#Postprocessing traces
-	for key in results.keys():
-		#Applying burn in
-		results[key] = results[key][burn:]
-		#Applying thinning
-		results[key] = results[key][::thin]
-
-	#Making trace plots and getting mean predictors
-	results['beta_mean'] =  np.zeros(p)
-	for j in range(p):
-		plt.plot(range(len(results['beta_trace'][:,j])), results['beta_trace'][:,j])
-		plt.xlabel('Iteration')
-		plt.ylabel('beta')
-		plt.savefig('trace'+str(j))
-		plt.close()
-		results['beta_mean'][j] = np.mean(results['beta_trace'][j])
+        lambda_diag = np.zeros(n)
+        for j in range(n):
+            lambda_diag[j] = stats.gamma.rvs((h+1)/2, (2/(h+w*(y[j] - np.transpose(x.iloc[j])@beta)**2)))
+        Lambda = np.diag(lambda_diag)
 
 
-	return results
+        results['beta_trace'][i,:] = beta
+        results['Lambda_trace'][i,:] = np.diagonal(Lambda)
+        results['w_trace'][i] = w
+
+        print('{:d} iterations out of {:d} complete.'.format(i,iterations))
+
+    #Postprocessing traces
+    for key in results.keys():
+        #Applying burn in
+        results[key] = results[key][burn:]
+
+
+    #Making trace plots and getting mean predictors
+    results['beta_mean'] =  np.zeros(p)
+    for j in range(p):
+        plt.plot(range(len(results['beta_trace'][:,j])), results['beta_trace'][:,j])
+        plt.xlabel('Iteration')
+        plt.ylabel('beta')
+        plt.savefig('trace'+str(j))
+        plt.close()
+        results['beta_mean'][j] = np.mean(results['beta_trace'][:,j])
+
+
+    return results
 
 
 
