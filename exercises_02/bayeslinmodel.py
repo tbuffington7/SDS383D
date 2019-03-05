f29ecd842669eebe45e5b202360f7403570eefe4
tbuffington7 <tyler.c.buffington@utexas.edu>
2019-03-04 18:51:35 -0600

    updated Bayesian linear model

--- exercises_02/bayeslinmodel.py
+++ exercises_02/bayeslinmodel.py
@@ -3,51 +3,89 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pdb
 
+
+def bayes_linear_model(x_data,y_data,K=None, Lambda=None, m_prior=None, add_intercept = True):
+    """
+
+    Returns fit parameters for a Bayseian linear model
+
+    Parameters
+    ----------
+    x: array_like with shape (nxp)
+        The x data
+    y: array_like with shape(nx1)
+        The y data
+    K: array_like with shape(pxp)
+        Prior precison matrix
+    Lambda: array_like with shape (nxn)
+
+    Returns
+    -------
+    beta_hat: array_like with shape (nx1)
+        An array of length two whose first entry is the fitted intercept and whose
+        second entry is the fitted slope from a Bayesian linear model
+    x: array_like with shape (nxp)
+        The X matrix that is multiplied with beta_hat to give predicted y values
+        Can be different than input argument x_data when it has a column of ones appended to it
+    """
+
+    #This is flipped, but it's a quirk of numpy
+    n = np.shape([x_data])[1]
+    p = np.shape([x_data])[0]
+
+    #If add_intercept is True, add a column of ones to the x matrix
+    if add_intercept == True:
+        p += 1
+        x = np.ones([n,p])
+        x[:,1:] = np.transpose([x_data])
+    else:
+        x = np.array(x_data)
+
+    #If K is not defined, give it very small values (close to OLS estimate)
+    if K is None:
+        K = np.diag(np.ones(p)*10**-12)
+
+    #If Lambda is not defined, just make it identity matrix
+    if Lambda is None:
+        Lambda = np.identity(n)
+    #if prior is not specified, make it zero for all parameters
+    if m_prior is None:
+        m_prior = np.zeros(p)
+
+    #Evaluting posterior
+    #First evaluate posterior precision matrix (defined as capital omega in the problem)
+    post_prec_mat = np.linalg.inv(K+x.transpose()@Lambda@x)
+
+    #Then evalute beta according to the derived equation
+    beta_hat = post_prec_mat@(x.transpose()@Lambda@y + K@m_prior)
+
+    return beta_hat, x
+
 #Loading the data into a pandas dataframe
 df = pd.read_csv('gdpgrowth.csv')
-#append an intercept column of ones
-df['intercept'] = np.ones(len(df))
-
-
 
 #Pulling x and y from the dataset
-x = df[ [ 'intercept', 'DEF60']].copy()
+x_data = df['DEF60']
 y = df['GR6096']
 
 
-
-
-#defining the priors
-n = len(y) #The number of observations
-p = len(x.columns)
-Lambda = np.identity(n) #The known matrix defined in the problem
-
 K = np.diag([.1,.1]) #A vague prior for the K precision
-m_prior = np.zeros(p)
 
-#Evaluting posterior
-#First evaluate precision matrix (defined as capital omega in the problem)
-prec_mat = np.linalg.inv(K+x.transpose()@Lambda@x)
+#Note the captial X has a column of ones for the intercept
+beta_hat, X = bayes_linear_model(x_data,y)
 
-#Then evalute beta according to the derived equation
-beta_hat = prec_mat@(x.transpose()@Lambda@y + K@m_prior)
-
-plt.scatter(x['DEF60'],y)
-plt.plot(x['DEF60'], x@beta_hat, linestyle='--', color='red')
+#Make scatterplot of raw data
+plt.scatter(x_data,y)
+#Let's define X as the vector that contains both the intercept and the predictor
+plt.plot(x_data, X@beta_hat, linestyle='--', color='red')
 
 
 #Also plot least squares fit
-plt.plot(x['DEF60'], np.poly1d(np.polyfit(x['DEF60'], y, 1))(x['DEF60']),linestyle='--', color='green' )
-
+plt.plot(x_data, np.poly1d(np.polyfit(x_data, y, 1))(x_data),linestyle='--', color='green' )
+plt.xlabel('Defense spending (fraction of GDP)')
+plt.ylabel('GDP growth rate')
+plt.legend(['Bayesian linear model', 'least squares fit'])
 
 
 plt.savefig('linear_bayes')
-
-
-
-
-
-if __name__ == "__main__":
-    import doctest
-    doctest.testmod()
 
--- exercises_03/kernel_smooth.py
+++ exercises_03/kernel_smooth.py
@@ -3,17 +3,104 @@ import matplotlib.pyplot as plt
 import pdb
 
 
+def gaussian_kernel(x):
+    """
+
+    Takes in an array of x values and returns corresponding Gaussian kernel values
+
+    Parameters
+    ----------
+     x: array_like
+        An array of x values
+
+    Returns
+    -------
+    gaussian_kernel(x): array_like
+        An array of same length as x with corresponding kernel values
+
+    """
+    return np.array([1.0/(2.0*np.pi)**.5 * np.exp(-i**2/2) for i in x])
+
+def weight_calc(x, x_star, h, kernel_func=gaussian_kernel):
+    """
+
+    Evaluates the normalized weights for based on a kernel function
+
+    Parameters
+    ----------
+    x: array_like
+        the array of x values from the data
+    x_star: float
+        The x value corresponding to the desired prediction location
+    h: float
+        The bandwidth
+    kernel_func: function
+        The desired kernel function. Default is gaussian
+
+
+    Returns
+    -------
+    weight_calc: array_like
+        The weight of each x data point for the desired prediction location
+    """
+    raw_weights = 1.0/h*kernel_func((x-x_star)/h)
+    norm_weights =  np.array(raw_weights/np.sum(raw_weights))
+    return norm_weights
+
+def pred(x,y, x_star, h, kernel_func=gaussian_kernel):
+
+    """
+
+    Makes predictions for an array x_star for a given dataset of x and y
+
+    Parameters
+    ----------
+    x: array_like
+        the array of x values from the data
+    y: array_like
+        the array of corresponding y values
+    x_star: float
+        The x value corresponding to the desired prediction location
+    h: float
+        The bandwidth
+    kernel_func: function
+        The desired kernel function. Default is gaussian
+
+    Returns
+    -------
+    y_star: array_like
+        The predicted values corresponding to x_star
+    """
+
+    y_star = np.zeros(len(x_star))
+    for i, x_val in enumerate(x_star):
+        weights = weight_calc(x, x_val,h, kernel_func)
+        y_star[i] = weights@y #matrix multiplciation
+
+    return y_star
+
 #Create a vector of x values
 x = np.linspace(0,10,100)
 #Use sin(x) as the function and add Gaussian noise
-y = np.sin(x) + np.random.normal(0,0.05)
+y = [np.sin(i) + np.random.normal(0,0.1) for i in x]
 
 #Define x_star: the x locations we're interested in predicting y
-x_star = np.linspace(0,10,10)
+x_star_vec = np.linspace(0,10,100)
 
 #initialize y_star: the corresponding predictions
-y_star = np.zeros(len(x_star))
+y_star_vec = np.zeros(len(x_star_vec))
 
-def gaussian_kernel(x):
-    #Takes an array x and returns array of Gaussian kernels of same length
-    return '[
+#Initializing array of h values for comparison
+h_vec = [0.05, 0.1, 1.0]
+
+#Making the plot of the noisy data outside of for loops
+plt.scatter(x,y, marker = 'x')
+for h in h_vec:
+    y_star_vec = pred(x,y,x_star_vec,h)
+    plt.plot(x_star_vec, y_star_vec)
+
+plt.legend(['h=0.05', 'h=0.1', 'h=1.0', 'raw_data'])
+plt.savefig('gaussian_kernel')
+plt.close()
+
+
